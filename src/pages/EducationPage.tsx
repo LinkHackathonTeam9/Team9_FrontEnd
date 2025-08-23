ff95b101788e30af2a261624400d8b4004052a6e
joshcho <sungho020426@naver.com>
2025-08-24 01:47:39 +0900

    Feat: 페이지 생성

---
+++ src/pages/EducationPage.tsx
@@ -0,0 +1,14 @@
+import Header from '@components/common/Header';
+import BottomNavBar from '@components/common/BottomNavBar';
+
+const EducationPage = () => {
+  return (
+    <>
+      <Header />
+      <div>EducationPage</div>
+      <BottomNavBar />
+    </>
+  );
+};
+
+export default EducationPage;
