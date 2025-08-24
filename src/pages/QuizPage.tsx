b30cde1bacb90712364235b522efe73cbb3bc309
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2025-08-24 17:13:03 +0900

    Remove useless files and folder

--- src/pages/QuizPage.tsx
+++ src/pages/QuizPage.tsx
@@ -1,10 +1,8 @@
-import Header from '@components/common/Header';
 import BottomNavBar from '@components/common/BottomNavBar';
 
 const QuizPage = () => {
   return (
     <>
-      <Header />
       <div>QuizPage</div>
       <BottomNavBar />
     </>
