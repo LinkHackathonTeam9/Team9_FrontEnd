c89b894bf9ffba04938621392e03d88566b0617b
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2025-08-24 17:13:15 +0900

    Add default style to bottom nav

--- src/components/common/BottomNavBar.tsx
+++ src/components/common/BottomNavBar.tsx
@@ -1,13 +1,27 @@
 import { NavLink } from 'react-router-dom';
+import styled from '@emotion/styled';
+
+const Container = styled.nav`
+  width: 100vw;
+  height: 60px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  background-color: #fff;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  border-top: 1px solid #ddd;
+`;
 
 const BottomNavBar = () => {
   return (
-    <nav>
+    <Container>
       <NavLink to="/">Home</NavLink>
       <NavLink to="/education">Education</NavLink>
       <NavLink to="/quiz">Quiz</NavLink>
       <NavLink to="/profile">Profile</NavLink>
-    </nav>
+    </Container>
   );
 };
 
