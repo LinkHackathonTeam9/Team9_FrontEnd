84a8c8936c8c91e1894980a31d58e185d7322621
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2025-08-28 21:55:24 +0900

    Fix remove useless variable

--- src/hooks/useApi.tsx
+++ src/hooks/useApi.tsx
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-const ENVIRONMENT = import.meta.env.VITE_ENVIRONMENT;
-
 function useApi() {
   const api = axios.create({
     baseURL: 'https://api.ggamja.o-r.kr/api',
